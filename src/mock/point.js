a77cf00f0837e85290b60fec0d1c67966983cfb5
Сергей Грищенков <grishenkov.sergei@gmail.com>
2020-08-04 23:37:24 -0500

    Начал ДЗ по личному проекту

---
+++ src/mock/point.js
@@ -0,0 +1,39 @@
+export const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+export const setTypeofPoint = () => {
+  const types = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check`, `Sightseeing`, `Restaurant`];
+  const randomIndex = getRandomInteger(0, types.length - 1);
+  return types[randomIndex];
+}
+
+export const setCities = () => {
+  const cities = [`Berlin`, `Paris`, `Tallin`, `Belfast`]
+  const randomIndex = getRandomInteger(0, cities.length - 1);
+  return cities[randomIndex]
+};
+
+const setExtras = () => {
+};
+
+const setDiscription = () => {
+  const discriptionText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
+
+  const splittedText = discriptionText.split(`.`);
+  const randomIndex = getRandomInteger(0, splittedText.length - 1);
+  return splittedText.slice(0, randomIndex);
+};
+
+export const point = {
+  setTypeofPoint,
+  setCities,
+  setExtras,
+  setDiscription,
+  photos: `http://picsum.photos/248/152?r=${Math.random()}`,
+};
+
+
