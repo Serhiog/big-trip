ad8e5b6b10c8233440149b25eae62cc5430ef352
Сергей Грищенков <grishenkov.sergei@gmail.com>
2020-08-08 22:06:52 -0500

    Коммит перед созвоном

--- src/view/tripEditorHeader.js
+++ src/view/tripEditorHeader.js
@@ -26,7 +26,7 @@ export const createTripEditorHeaderTemplate = (points) => {
 
         <div class="event__type-item">
           <input id="event-type--1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="taxi">
-          <label class="event__type-label  event__type-label--taxi" for="event-type--1"></label>
+          <label class="event__type-label  event__type-label--taxi" for="event-type-taxi-1">Taxi</label>
         </div>
 
         <div class="event__type-item">
